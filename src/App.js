b3c5ac99dbf6a649ae2e48c0a9ab2c96c241b197
Chris House <chrishouse83@protonmail.com>
2024-06-30 20:19:54 -0500

    change date formatting

--- src/App.js
+++ src/App.js
@@ -35,34 +35,35 @@ function App() {
 			});
 	}, [options]);
 
-  if (false) {
-    setReleases();
-  }
+	if (false) {
+		setReleases();
+	}
 
-	// const months = {
-	// 	January: "01",
-	// 	February: "02",
-	// 	March: "03",
-	// 	April: "04",
-	// 	May: "05",
-	// 	June: "06",
-	// 	July: "07",
-	// 	August: "08",
-	// 	September: "09",
-	// 	October: "10",
-	// 	November: "11",
-	// 	December: "12"
-	// };
+	const months = {
+		January: "01",
+		February: "02",
+		March: "03",
+		April: "04",
+		May: "05",
+		June: "06",
+		July: "07",
+		August: "08",
+		September: "09",
+		October: "10",
+		November: "11",
+		December: "12"
+	};
 
 	releases.forEach((item) => {
 		// construct ISO date from given date
-		// let splitDate = item.date.split(" ");
-		// let month = months[splitDate[0]];
-		// let date = splitDate[1].replace("th", "").replace("rd", "").replace("nd", "").replace("st", "");
-		// let year = splitDate[2];
-		// let newDate = new Date(year + "-" + month + "-" + date);
-		// newDate = newDate.toISOString();
-		// item.isoDate = newDate;
+		let splitDate = item.date.split(" ");
+		let month = months[splitDate[0]];
+		let date = splitDate[1].replace("th", "").replace("rd", "").replace("nd", "").replace("st", "");
+		let year = splitDate[2];
+		let newDate = new Date(date + " " + month + " " + year + " 00:00 UTC");
+		console.log(newDate);
+		newDate = newDate.toISOString();
+		item.isoDate = newDate;
 		item.albumTitle = item.album.replace(/(<([^>]+)>)/gi, "");
 	});
 
